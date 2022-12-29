a4fc14960141f68d14c1a2a77d54f4fd44ad81c2
shoaibakhtar1010 <shoaibakhtar19r@gmail.com>
2022-12-29 14:18:17 +0530

    client metadata changed ingress

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator by Shoaibakhtar
+            Fib Calculator by Shoaibakhtar using K8S
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
